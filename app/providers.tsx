109b18342c5bcf67cc99e4f76c01367417bb0880
bavena95 <104534458+bavena95@users.noreply.github.com>
2025-06-29 14:39:46 -0700

    Updated providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,12 @@
-"use client";
+"use client"
 
-import React from "react";
-import { StackProvider } from "@stackframe/stack";
-import { stackClientApp } from "@/lib/stack-client";
-import { ThemeProvider } from "@/components/theme-provider"; // Supondo que você tenha este componente para o tema
+import type React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  // O ThemeProvider é opcional, mas se você o tiver, o StackProvider deve vir dentro dele.
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <StackProvider app={stackClientApp}>
-            {children}
-        </StackProvider>
+      {children}
     </ThemeProvider>
-  );
+  )
 }
