3ad8776c0f250672fca4dfbd33533ec27480dd77
bavena95 <104534458+bavena95@users.noreply.github.com>
2025-06-29 14:48:10 -0700

    Fix StackProvider context error

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,15 @@
 
 import type React from "react"
 import { ThemeProvider } from "@/components/theme-provider"
+import { StackProvider } from "@stackframe/stack"
+import { stackClientApp } from "@/lib/stack-client"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
-    </ThemeProvider>
+    <StackProvider app={stackClientApp}>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        {children}
+      </ThemeProvider>
+    </StackProvider>
   )
 }
